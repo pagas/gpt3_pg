21377c17019e07e63c0aae4385bef18c4928daa7
paulius <paulius@2simple.com>
2023-02-13 19:56:05 +0000

    create and import container files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {Article, Brand, CTA} from "./components";
+import {Navbar, Brand, CTA} from "./components";
+import {Footer, Blog, Possibility, Features, WhatGPT, Header} from "./containers"
 
 const App = () => {
     return (
-        <div>
-            <h1>GPT-3</h1>
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar/>
+                <Header/>
+            </div>
+            <Brand/>
+            <WhatGPT/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
         </div>
     );
 };
